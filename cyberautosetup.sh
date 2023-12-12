4528c8824d28c45b92db3eec4f7e05d75bdc2ed1
Ross Thomas <106878841+Cyb3rHack3r@users.noreply.github.com>
2023-12-12 21:08:55 +1100

    Add files via upload

--- cyberautosetup.sh
+++ cyberautosetup.sh
@@ -6,12 +6,24 @@
         REDBKGSTART='\e[1;41m'
         BKGEND='\e[0m'
 
+    #user variables
+
 #echo script info
     echo -e "$REDBKGSTART ~~CYBERS AUTO SETUP SCRIPT~~ $BKGEND"
     sleep 3
 
+#prompt for username
+    read -p "WHAT IS YOUR USERNAME? " USERNAME
+    read -p "You have entered | $USERNAME | Continue (y/n)?" NAMECHECK
+        if [ "$NAMECHECK" = "y" ]; then
+        echo "OK, SCRIPT WILL START NOW";
+        else
+        echo "RERUN SCRIPT TO TRY AGAIN";
+        exit
+        fi
+
 #install desired packages with yay
-    yay -S --noconfirm steam brave-bin kvantum code virtualbox virtualbox-guest-utils partitionmanager screenfetch plex-media-server htop
+    yay -S --noconfirm steam brave-bin kvantum code virtualbox virtualbox-guest-utils partitionmanager screenfetch plex-media-server htop libotf
     echo -e "$REDBKGSTART ~~PACKAGES INSTALLED~~ $BKGEND"
     sleep 3
 
@@ -23,20 +35,20 @@
 #sets up drive mounts and permissions
 
     #makes mounting directories"
-        sudo mkdir /run/media/cyberhacker/mntsda3
-        sudo mkdir /run/media/cyberhacker/mntsdb2
+        sudo mkdir /run/media/$USERNAME/mntsda3
+        sudo mkdir /run/media/$USERNAME/mntsdb2
         echo -e "$REDBKGSTART ~~MOUNT POINTS MADE~~ $BKGEND"
         sleep 3
 
-    #gives permissions to cyberhacker user and group to the above created directories
-        sudo chown -R cyberhacker:cyberhacker /run/media/cyberhacker/
-        sudo chown -R cyberhacker:cyberhacker /home/cyberhacker/
-        sudo chmod -R a+rwx /run/media/cyberhacker/
-        sudo chmod -R a+rwx /home/cyberhacker
+    #gives permissions to $USERNAME user and group to the above created directories
+        sudo chown -R $USERNAME:$USERNAME /run/media/$USERNAME/
+        sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/
+        sudo chmod -R a+rwx /run/media/$USERNAME/
+        sudo chmod -R a+rwx /home/$USERNAME
 
 
-            #adds user plex to group cyberhacker
-                sudo usermod -a -G cyberhacker plex 
+            #adds user plex to group $USERNAME
+                sudo usermod -a -G $USERNAME plex 
 
         echo -e "$REDBKGSTART ~~PERMISSIONS SET~~ $BKGEND"
         sleep 3
@@ -54,9 +66,9 @@
             TARGETFSTAB="/etc/fstab"
 
             #terms to enter if grep is non 0 for the check terms
-            ENTERTEXTSDA3="/dev/sda3                                   /run/media/cyberhacker/mntsda3   btrfs                defaults                               0 0"
+            ENTERTEXTSDA3="/dev/sda3                                   /run/media/$USERNAME/mntsda3   btrfs                defaults                               0 0"
             ENTERTEXTSDB1="/dev/sdb1                                   /home                            btrfs                defaults                               0 0"
-            ENTERTEXTSDB2="/dev/sdb2                                   /run/media/cyberhacker/mntsdb2   btrfs                defaults                               0 0"
+            ENTERTEXTSDB2="/dev/sdb2                                   /run/media/$USERNAME/mntsdb2   btrfs                defaults                               0 0"
 
         #write sda3 mount in fstab
             grep -q "$CHECKTEXTSDA3" $TARGETFSTAB || echo "$ENTERTEXTSDA3" | sudo tee -a $TARGETFSTAB > /dev/null
@@ -75,7 +87,30 @@
     
     echo -e "$REDBKGSTART ~~SERVICES STARTED AND ENABLED~~ $BKGEND"
     sleep 3
-    
-#echo the all done message
 
-    echo -e "$REDBKGSTART ~~AUTO SETUP COMPLETE~~ $BKGEND"
+#setup theming and customization stuff
+
+    #clone theming repos
+        mkdir /home/$USERNAME/GitRepos
+        cd /home/$USERNAME/GitRepos
+        git clone https://github.com/vinceliuice/Orchis-kde.git
+        git clone https://github.com/vinceliuice/Layan-kde.git
+        git clone https://github.com/yeyushengfan258/Reversal-icon-theme.git
+
+    #install theme related stuff
+        cd /home/$USERNAME/GitRepos/Orchis-kde/
+        ./install.sh
+        cd /home/$USERNAME/GitRepos/Layan-kde/
+        ./install.sh
+        cd /home/$USERNAME/GitRepos/Reversal-icon-theme/
+        ./install.sh
+
+    #download OTF fonts
+        sudo mkdir /usr/share/fonts/opentype
+        cd /usr/share/fonts/opentype
+        sudo git clone https://github.com/sahibjotsaggu/San-Francisco-Pro-Fonts.git
+
+echo -e "$REDBKGSTART ~~CUSTOMIZATIONS INSTALLED~~ $BKGEND"
+
+#echo the all done message
+     echo -e "$REDBKGSTART ~~AUTO SETUP COMPLETE~~ $BKGEND"
